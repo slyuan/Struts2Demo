dc0f060a55f5ffcc625f19ae7df2c169da58c873
slyuan <bossyslgood@163.com>
2016-10-28 14:48:23 +0800

    代码格式化

--- src/org/crazyit/app/action/LoginAction.java
+++ src/org/crazyit/app/action/LoginAction.java
@@ -4,45 +4,42 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
- * Description:
- * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
- * <br/>Copyright (C), 2001-2014, Yeeku.H.Lee
- * <br/>This program is protected by copyright laws.
- * <br/>Program Name:
- * <br/>Date:
+ * Description: <br/>
+ * 网站: <a href="http://www.crazyit.org">疯狂Java联盟</a> <br/>
+ * Copyright (C), 2001-2014, Yeeku.H.Lee <br/>
+ * This program is protected by copyright laws. <br/>
+ * Program Name: <br/>
+ * Date:
+ * 
  * @author Yeeku.H.Lee kongyeeku@163.com
  * @version 1.0
  */
-public class LoginAction extends ActionSupport
-{
+public class LoginAction extends ActionSupport {
 	// 定义封装请求参数的username和password成员变量
 	private String username;
-	private String password;	
-	public String getUsername()
-	{
+	private String password;
+
+	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username)
-	{
+
+	public void setUsername(String username) {
 		this.username = username;
-	}	
-	public String getPassword()
-	{
+	}
+
+	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password)
-	{
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	// 定义处理用户请求的execute方法
-	public String execute() throws Exception
-	{
+	public String execute() throws Exception {
 		// 当username为crazyit.org，password为leegang时即登录成功
-		if (getUsername().equals("slyuan")
-			&& getPassword().equals("slyuan") )
-		{
-			ActionContext.getContext().getSession()
-				.put("user" , getUsername());
+		if (getUsername().equals("slyuan") && getPassword().equals("slyuan")) {
+			ActionContext.getContext().getSession().put("user", getUsername());
 			return SUCCESS;
 		}
 		return ERROR;
